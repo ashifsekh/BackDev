88894b9291a164b06ffb6437632aedfffc90089d
ashifsekh <aashifsekhh@gmail.com>
2026-02-14 21:18:41 +0530

    added the about

--- EXPRESS/1.HTTP Requests/index.js
+++ EXPRESS/1.HTTP Requests/index.js
@@ -4,6 +4,9 @@ const app = express();
 app.get("/", (req, res) => {
     res.send('<h1>Welcome to the Home page</h1>')
 })
+app.get("/about", (req, res) => {
+    res.send('<h1>About me</h1> <p1>This is Ashis Sekh and I am a student</p>')
+})
 
 app.listen(3000, () => {
     console.log("Server running on port 3000");
