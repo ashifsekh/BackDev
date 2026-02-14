ac813fc9c366339454f9803a248284ab35e15cb6
ashifsekh <aashifsekhh@gmail.com>
2026-02-14 21:14:13 +0530

    added the get res

--- EXPRESS/1.HTTP Requests/index.js
+++ EXPRESS/1.HTTP Requests/index.js
@@ -1,6 +1,10 @@
 import express from "express";
 const app = express();
 
+app.get("/", (req, res) => {
+    res.send('<h1>Welcome to the Home page</h1>')
+})
+
 app.listen(3000, () => {
     console.log("Server running on port 3000");
 });
