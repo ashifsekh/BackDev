6242060746b010fd3d2283b0c11b4da770e0c63d
ashifsekh <aashifsekhh@gmail.com>
2026-02-15 11:10:44 +0530

    feat: add body-parser middleware and update its version

--- EXPRESS/3.Middleware/index1.js
+++ EXPRESS/3.Middleware/index1.js
@@ -1,13 +1,23 @@
 import express from "express";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
+import bodyParser from "body-parser";
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express();
 const port = 3000;
 
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/public/index.html");
+});
+
+app.post("/submit", (req, res) => {
+  const street = req.body.street;
+  const pet = req.body.pet;
+  console.log(`Street: ${street}, Pet: ${pet}`);
+  res.send(`<h1>Your band name is: ${street} ${pet}</h1>`);
 });
 
 app.listen(port, () => {
