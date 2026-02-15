c19383825815959b087056a37783d9afbc7069c2
ashifsekh <aashifsekhh@gmail.com>
2026-02-15 14:27:53 +0530

    fix: remove redundant body-parser import and update middleware usage

--- EXPRESS/3.Middleware/index1.js
+++ EXPRESS/3.Middleware/index1.js
@@ -1,13 +1,15 @@
+import bodyParser from "body-parser";
 import express from "express";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
-import bodyParser from "body-parser";
+// import bodyParser from "body-parser";
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express();
 const port = 3000;
 
-app.use(bodyParser.urlencoded({ extended: true }));
+//app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/public/index.html");
