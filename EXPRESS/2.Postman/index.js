2062b00030e177c565b1028bd459a615ed273d31
ashifsekh <aashifsekhh@gmail.com>
2026-02-15 09:14:45 +0530

    feat: add user registration and management endpoints

--- EXPRESS/2.Postman/index.js
+++ EXPRESS/2.Postman/index.js
@@ -4,8 +4,26 @@ const app = express();
 app.get("/", (req, res) => {
     res.send('<h1>Welcome to the Home page</h1>')
 })
+
 app.get("/about", (req, res) => {
     res.send('<h1>About me</h1> <p1>This is Ashis Sekh and I am a student</p>')
+})
+
+app.post("/register", (req, res) => {
+    // will do something with the data and then send a response
+    res.sendStatus(201).json({ message: "User registered successfully" });
+})
+
+app.put("/user/ashif", (req, res) => {
+    res.sendStatus(200).json({ message: "User updated successfully" });
+})
+
+app.patch("/user/ashif", (req, res) => {
+    res.sendStatus(200).json({ message: "User updated successfully" });
+}
+
+app.delete("/user/ashif", (req, res) => {
+    res.sendStatus(200).json({ message: "User deleted successfully" });
 })
 
 app.listen(3000, () => {
