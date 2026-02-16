3c0d3aab36b862e834e735680325f23f048a731c
ashifsekh <aashifsekhh@gmail.com>
2026-02-16 12:45:34 +0530

    feat: set up Express server with routes for password submission and file serving

--- EXPRESS/3.1 Secrets Project/index.js
+++ EXPRESS/3.1 Secrets Project/index.js
@@ -2,3 +2,30 @@
 //Make sure you have installed all the dependencies with "npm i".
 //The password is ILoveProgramming
 
+import express from "express";
+import {dirname} from "path";
+import { fileURLToPath } from "url";
+
+const __dirnaem = dirname(fileURLToPath(import.meta.url));
+
+const app = express();
+const port = 3000;
+
+app.use(express.urlencoded({extended: true}));
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirnaem + "/public/index.html");
+});
+
+app.post("/submit", (req, res) => {
+    const password = req.body.password;
+    if (password === "ILoveProgramming") {
+        res.sendFile(__dirnaem + "/public/secret.html");
+    } else {
+        res.send('<h1>Wrong password! Try again.</h1><a href="/">Go back</a>');
+    }
+});
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+});
