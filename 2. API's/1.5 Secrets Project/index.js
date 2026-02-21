7ac24fb3df29876f71726340e57cb791a3c1c33a
ashifsekh <aashifsekhh@gmail.com>
2026-02-21 06:58:52 +0530

    feat: update main.css hover color for card and refactor index.js for error handling

--- 2. API's/1.5 Secrets Project/index.js
+++ 2. API's/1.5 Secrets Project/index.js
@@ -11,3 +11,27 @@
 // secret and the username of the secret.
 
 // 6. Listen on your predefined port and start the server.
+
+import express from "express";
+import axios from "axios";
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", async (req, res) => {
+    try {
+        const response = await axios.get("https://secrets-api.appbrewery.com/random");
+        console.log(response.data);
+        res.render("index.ejs", { secret: response.data.secret, user: response.data.username });
+    } catch (error) {
+        console.error("Error fetching secret:", error);
+        res.render("index.ejs", { secret: "Error fetching secret", username: "" });
+    }
+});
+
+app.listen(PORT, () => {
+    console.log('Server is running on port ' + PORT);
+});
